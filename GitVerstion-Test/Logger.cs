4db291dde2d70795acec3cb88e194c7a96eae1a3
shiping an <itrice@163.com>
2022-07-18 15:00:19 +0800

    +semver: minor test

--- GitVerstion-Test/Logger.cs
+++ GitVerstion-Test/Logger.cs
@@ -5,6 +5,7 @@ public class Logger
     {
         // +semver: major #passed
         // +semver: feature  # not passed
+        // +semver: minor 
         // +semver: fix 
         Console.WriteLine(text);
     }
