2346faaab0a2b7ba39c8751e87f427473fb47a39
shiping an <itrice@163.com>
2022-07-18 15:49:20 +0800

    +semver: fix

--- GitVerstion-Test/Logger.cs
+++ GitVerstion-Test/Logger.cs
@@ -7,7 +7,7 @@ public class Logger
         // +semver: breaking #no passed
         // +semver: feature # passed
         // +semver: minor #no passed
-        // +semver: fix   
+        // +semver: fix    
 
 
 
